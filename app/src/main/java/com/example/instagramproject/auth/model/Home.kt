8d027dfd77d3e94d6f4f88610207d514f9f469b2
mehdi <dehghani_m@rocketmail.com>
2023-06-09 17:10:05 +0430

    make model home and reel

---
+++ app/src/main/java/com/example/instagramproject/auth/model/Home.kt
@@ -0,0 +1,31 @@
+package com.example.instagramproject.auth.model
+
+data class Post(
+    val id: Int,
+    val image: String,
+    val user: User,
+    val isLiked: Boolean = false,
+    val likesCount: Int,
+    val commentsCount: Int,
+    val timeStamp: Long
+)
+
+data class Story(
+    val image: String,
+    val name: String,
+    val isSeen: Boolean = false
+)
+
+val names = arrayOf(
+    "Shivam",
+    "Anuj",
+    "Samson",
+    "Prajwal",
+    "Arijit",
+    "Rajiv",
+    "Suraj",
+    "David",
+    "Tushar",
+    "Jyotirmoy",
+    "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve"
+)
