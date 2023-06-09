5d1857f3c553cea99c2c9127f0eb20d5803c2852
mehdi <dehghani_m@rocketmail.com>
2023-06-09 17:19:52 +0430

    make reels repository

---
+++ app/src/main/java/com/example/instagramproject/auth/data/ReelsRepository.kt
@@ -0,0 +1,46 @@
+package com.example.instagramproject.auth.data
+
+import com.example.instagramproject.auth.model.Reel
+import com.example.instagramproject.auth.model.User
+import com.example.instagramproject.auth.model.names
+
+object ReelsRepository {
+
+    private val reels = arrayListOf<Reel>()
+
+    private fun populateReels(){
+        val _reels = ArrayList<Reel>()
+        (0..9).forEach{ index ->
+            val post = Reel(
+                id = index + 1,
+                video = videos[index],
+                user = User(
+                    name = names[index],
+                    username = names[index],
+                    image = "https://randomuser.me/api/portraits/men/${index + 1}.jpg"
+                ),
+                likesCount = index + 100,
+                commentsCount = index + 20
+            )
+            _reels.add(post)
+        }
+        reels.clear()
+        reels.addAll(_reels)
+    }
+
+    private val videos = listOf(
+        "food.mp4",
+        "soap-bubbles.mp4",
+        "castle.mp4",
+        "lake.mp4",
+        "icecream.mp4",
+        "soap-bubbles.mp4",
+        "castle.mp4",
+        "lake.mp4",
+        "icecream.mp4",
+        "soap-bubbles.mp4",
+        "castle.mp4",
+        "lake.mp4",
+        "icecream.mp4",
+    )
+}
