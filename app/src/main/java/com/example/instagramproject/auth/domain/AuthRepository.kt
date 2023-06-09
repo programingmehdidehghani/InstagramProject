ef63db8fbe0e7270c32b43531a846b5a1f249b19
mehdi <dehghani_m@rocketmail.com>
2023-06-09 14:16:59 +0430

    make repository

---
+++ app/src/main/java/com/example/instagramproject/auth/domain/AuthRepository.kt
@@ -0,0 +1,26 @@
+package com.example.instagramproject.auth.domain
+
+import android.net.Uri
+import com.example.instagramproject.auth.data.dto.CreateUserDto
+import com.google.firebase.auth.FirebaseUser
+
+interface AuthRepository {
+
+    suspend fun createUserWithEmailAndPassword(email:String , password:String) : FirebaseUser?
+
+    suspend fun signInWithEmailAndPassword(email: String , password: String): FirebaseUser?
+
+    suspend fun checkUsernameAvailability(username: String): Boolean
+
+    suspend fun signOut() : FirebaseUser?
+
+    suspend fun getUser() : FirebaseUser?
+
+    suspend fun sendPasswordResetEmail(email :String): Boolean
+
+    suspend fun verifyPasswordResetCode(code: String)
+
+    suspend fun saveUserProfile(createUserDto: CreateUserDto): Boolean
+
+    suspend fun uploadProfileImage(imageUri: Uri): String
+}
