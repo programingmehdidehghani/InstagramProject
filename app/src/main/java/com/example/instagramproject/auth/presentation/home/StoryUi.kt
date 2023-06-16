126c943f3e282c61ae7bdb7e75294b5ab23ae0b9
mehdi <dehghani_m@rocketmail.com>
2023-06-16 11:36:32 +0430

    start make storyImage

---
+++ app/src/main/java/com/example/instagramproject/auth/presentation/home/StoryUi.kt
@@ -0,0 +1,17 @@
+package com.example.instagramproject.auth.presentation.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun StoryImage(imageUrl: String) {
+    val shape = CircleShape
+    Box(
+        modifier = Modifier
+            .
+    ){
+
+    }
+}
---
+++ app/src/main/java/com/example/instagramproject/common/components/Config.kt
@@ -0,0 +1,10 @@
+package com.example.instagramproject.common.components
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+val verticalPadding = 12.dp
+val horizontalPadding = 10.dp
+val bottomBarHeight = 50.dp
+
+fun Modifier
