4192d1d6033fb56f3507cf049c45721d407c028f
mehdi <dehghani_m@rocketmail.com>
2023-07-07 18:02:40 +0330

    start make postview

---
+++ app/src/main/java/com/example/instagramproject/auth/presentation/home/PostUi.kt
@@ -0,0 +1,14 @@
+package com.example.instagramproject.auth.presentation.home
+
+import androidx.compose.runtime.Composable
+import com.example.instagramproject.auth.model.Post
+
+
+@Composable
+fun PostView(
+    post: Post,
+    onDoubleClick: (Post) -> Unit,
+    onLikeToggle: (Post) -> Unit
+){
+
+}
