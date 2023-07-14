14b7f76e8e106202651c5d451724df1dd0fe8a40
mehdi <dehghani_m@rocketmail.com>
2023-07-14 17:12:28 +0330

    continue make class home

--- app/src/main/java/com/example/instagramproject/auth/presentation/home/Home.kt
+++ app/src/main/java/com/example/instagramproject/auth/presentation/home/Home.kt
@@ -1,5 +1,6 @@
 package com.example.instagramproject.auth.presentation.home
 
+import android.annotation.SuppressLint
 import android.net.wifi.hotspot2.pps.HomeSp
 import android.widget.ExpandableListView.OnChildClickListener
 import android.widget.ImageButton
@@ -12,13 +13,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,18 +33,45 @@ import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.example.instagramproject.R
+import com.example.instagramproject.auth.data.PostsRepository
+import com.example.instagramproject.auth.data.StoriesRepository
 import com.example.instagramproject.auth.model.Post
 import com.example.instagramproject.auth.model.Story
+import kotlinx.coroutines.launch
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Home() {
     val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
-        topBar =
-    ) {
-
+        topBar = { Toolbar() }) {
+        val posts by PostsRepository.posts
+        val stories by StoriesRepository.observeStories()
+        
+        LazyColumn {
+            item {
+                StoriesSection(stories)
+                Divider()
+            }
+            itemsIndexed(posts){ _, post ->
+                com.example.instagramproject.auth.presentation.home.Post(
+                    post,
+                    onDoubleClick = {
+                       coroutineScope.launch {
+                           PostsRepository.performLike(post.id)
+                       }
+                    },
+                    onLikeToggle = {
+                        coroutineScope.launch {
+                            PostsRepository.toggleLike(post.id)
+                        }
+                    }
+                )
+            }
+        }
     }
 }
 
@@ -74,13 +106,14 @@ fun Toolbar() {
 @Composable
 fun StoriesSection(stories: List<Story>) {
     Column {
-
+       storiesList(stories)
+        Spacer(modifier = Modifier.height(10.dp))
     }
 
 }
 
 @Composable
-fun StoriesList(stories: List<Story>) {
+fun storiesList(stories: List<Story>) {
     LazyRow {
         itemsIndexed(stories) { index, story ->
 
