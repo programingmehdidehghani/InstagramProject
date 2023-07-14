8f7702efe4e635a396af835c123499e5dd3f0302
mehdi <dehghani_m@rocketmail.com>
2023-07-14 13:06:17 +0330

    add destonation

---
+++ app/src/main/java/com/example/instagramproject/auth/presentation/login/LoginScreen.kt
@@ -0,0 +1,14 @@
+package com.example.instagramproject.auth.presentation.login
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphNavigator
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator,
+
+    ){
+
+}
