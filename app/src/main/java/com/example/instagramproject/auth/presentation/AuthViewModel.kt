789255b7606ad7a603f54a676c6b6d942365660f
mehdi <dehghani_m@rocketmail.com>
2023-07-14 15:41:52 +0330

    make funtion createUser

--- app/src/main/java/com/example/instagramproject/auth/presentation/AuthViewModel.kt
+++ app/src/main/java/com/example/instagramproject/auth/presentation/AuthViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.instagramproject.auth.presentation
 
+import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.instagramproject.auth.data.dto.CreateUserDto
 import com.example.instagramproject.auth.domain.AuthRepository
 import com.example.instagramproject.auth.domain.AuthValidator
+import com.google.firebase.firestore.FieldValue
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
@@ -39,6 +42,45 @@ class AuthViewModel @Inject constructor(
             }
         }
 
+    }
+
+
+    fun createUser(imageUri: Uri, createUserDto: CreateUserDto) = viewModelScope.launch {
+         isLoading = true
+        try {
+            val usernameAvailability = authRepository.checkUsernameAvailability(createUserDto.username)
+            if (!usernameAvailability){
+                isLoading = false
+                eventChannel.send(ResultEvents.OnError("Username entered already exist, try another one."))
+            } else {
+                val firebaseUser = authRepository.createUserWithEmailAndPassword(
+                    createUserDto.email,
+                    createUserDto.password
+                )
+                firebaseUser?.let { user ->
+                    val imageUri = authRepository.uploadProfileImage(imageUri)
+
+                    val updateUser = createUserDto.copy(
+                        uid = user.uid,
+                        createdDate = FieldValue.serverTimestamp(),
+                        imageUrl = imageUri,
+                        password = ""
+                    )
+                    authRepository.saveUserProfile(updateUser)
+
+                }
+                isLoading = false
+                eventChannel.send(ResultEvents.OnSuccess("User created, kindly verify your mail to login"))
+            }
+
+        }catch (e: Exception){
+            isLoading = false
+            eventChannel.send(
+                ResultEvents.OnError(
+                    e.localizedMessage ?: "Unable to Create User, try again."
+                )
+            )
+        }
     }
 
     fun signIn(email: String, password: String) = viewModelScope.launch{
